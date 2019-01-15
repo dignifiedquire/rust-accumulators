8d9b4e0c43eeffba00c906a57d00b748829a2843
goldenMetteyya <hello@organism.ai>
2019-01-15 19:43:28 +0300

    Update traits.rs

--- src/traits.rs
+++ src/traits.rs
@@ -150,11 +150,6 @@ pub trait DynamicVectorCommitment: StaticVectorCommitment {
 /// class group — can be chosen without the need for a trusted setup, which is a major advantage for
 /// using class groups in applications requiring groups of unknown order.
 pub trait PrimeGroup {
-    #[cfg(feature = "class_group")]
-    /// Generates the Prime elements from the group that is used via a seed
-    /// Returns first the prime and second the generator used
-    fn generate_primes(seed: &[u8], int_size_bits: usize) -> Result<(BigUint, BigUint), Error>;
-
     #[cfg(feature = "rsa_group")]
     /// Generates the Prime elements from the group that is used
     /// Returns first the prime and second the generator used
